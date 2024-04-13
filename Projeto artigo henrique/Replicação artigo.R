b7162e2a5169d4597cae6826b5ab8928beabb3f4
Estevao Miguel Cardoso da SIlva <estevaomcs@gmail.com>
2024-04-13 13:45:48 -0300

    t

--- Projeto artigo henrique/Replicação artigo.R
+++ Projeto artigo henrique/Replicação artigo.R
@@ -18,7 +18,8 @@ model1 <- lm(Treatment ~ yearsedu_brinjalgrwer + age_brinjalgrwer + years_farmer
 coeftest(model1, vcov = vcovHC(model1, type = "HC1"))
 model2 <- lm(attrition ~ Treatment + yearsedu_brinjalgrwer + age_brinjalgrwer + years_farmer + opland + wealthV2 + yield_bl + pesticide_cost_bl, data = Balance_Attrition)
 coeftest(model2, vcov = vcovHC(model2, type = "HC1"))
-write.csv(summary(model1)$coefficients, file = file.path(results, "Table2.csv"))
+write.csv(summary(model1)$coefficients, file = file.path(results, "Table2.csv")) ss
+
 
 
 
